cf2a5c790b11acd2f719ccf1a48ca896d998d4db
Adam Kuchar <adamkuchar10@gmail.com>
2026-03-07 14:48:19 +0100

    Init commit Lab4

---
+++ Lab4/privatekey.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from random import randint
+from signature import Signature
+
+from s256point import S256Point
+
+A = 0
+B = 7
+P = 2**256 - 2**32 - 977
+N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+
+G = S256Point(
+    0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+    0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8,
+)
+
+class PrivateKey:
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        return f"{self.secret:x}".zfill(64)
+
+    def sign(self, z):
+    # TODO 1: Sign
+        raise NotImplementedError
+
+
+class PrivateKeyTest(TestCase):
+    def test_sign(self):
+        pk = PrivateKey(randint(0, 2**256))
+        z = randint(0, 2**256)
+        sig = pk.sign(z)
+        self.assertTrue(pk.point.verify(z, sig))
